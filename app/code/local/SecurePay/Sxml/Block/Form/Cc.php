5c1d224a0110d1bf0d21e1b25eb476ee16d24f13
Ben Turner <ben@aligent.com.au>
2014-08-18 17:23:14 +0930

    Add default securepay logo and default template files to securepay

--- app/code/local/SecurePay/Sxml/Block/Form/Cc.php
+++ app/code/local/SecurePay/Sxml/Block/Form/Cc.php
@@ -6,6 +6,13 @@ class SecurePay_Sxml_Block_Form_Cc extends Mage_Payment_Block_Form_Cc
     {
         parent::_construct();
         $this->setTemplate('sxml/form/cc.phtml');
+
+        if (!Mage::app()->getStore()->isAdmin()) {
+            $mark = Mage::getConfig()->getBlockClassName('core/template');
+            $mark = new $mark;
+            $mark->setTemplate('securepay/mark.phtml');
+            $this->setMethodLabelAfterHtml($mark->toHtml());
+        }
     }
 } 
 
